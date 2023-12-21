1af40441663c209d3947f869608097057a738bf8
Ray <raybartlett@tuta.com>
2023-12-21 13:27:22 -0500

    verified worst tracks

--- worst_tracks.py
+++ worst_tracks.py
@@ -1,57 +1,58 @@
 worst_tracks_2016 = {
     "drake - one dance",
-    "maroon 5 - don't wanna know",
+    "maroon 5 & kendrick lamar - don't wanna know (feat. kendrick lamar)",
     "she & him - happy holiday",
-    "rob zombie - well, everybody's fucking in a ufo",
-    "pussy riot - straight outta vagina",
-    "charlie puth - marvin gaye",
-    "macklemore & ryan lewis - white privilege ii",
+    "rob zombie - well, everybody's fucking in a u.f.o.",
+    "pussy riot & desi mo & leikeli47 - straight outta vagina (feat. desi mo & leikeli47)",
+    "charlie puth & meghan trainor - marvin gaye (feat. meghan trainor)",
+    "macklemore & ryan lewis & macklemore & ryan lewis & Jamila Woods - white privilege ii (feat. jamila woods)",
     "birdman - respek",
     "disturbed - the sound of silence",
-    "the black eyed peas - where's the love",
+    "the black eyed peas & the world - #wheresthelove",
     "3oh!3 - my dick",
     "b.o.b. - flatline",
-    "lena dunham - sensual pantsuit anthem",
-    "corey feldman - go 4 it",
-    "le tigre - i'm with her",
+    "b.o.b. - flatline pt =",  # duplicate?
+    "lena dunham - sensual pantsuit anthem",  # not on spotify
+    "corey feldman & snoop dogg - go 4 it ! (feat. snoop dogg)",
+    "le tigre - i'm with her",  # not on spotify
 
 }
 
 worst_tracks_2017 = {
     "weezer - feels like summer",
     "mndr & scissor sisters - swerlk",
-    "machine gun kelly - at my best",
+    "machine gun kelly & hailee steinfeld- at my best (feat. hailee steinfeld)",
     "iggy azalea - mo bounce",
     "fall out boy - young and menace",
     "lil yachty - bring it back",
     "will smith - get lit",
-    "ricegum - it's everynight sis",
-    "jake paul - it's everyday bro",
+    "ricegum - its every night sis",
+    "jake paul, team 10 - it's everyday bro",
     "hopsin - happy ending",
 }
 
 worst_tracks_2018 = {
     "noah cyrus & lil xan - live or die",
-    "lil dicky - freaky friday ft. chris brown",
+    "lil dicky, chris brown - freaky friday (feat. chris brown)",
     "bad religion - the kids are alt-right",
-    "6ix9ine - bebe ft. anuel aa",
-    "pitbull - ocean to ocean ft. rhea",
-    "kanye west - ye vs the people",
+    "6ix9ine & anuel aa - bebe (feat. anuel aa)",
+    "pitbull & rhea - ocean to ocean (feat. rhea)",
+    "kanye west - ye vs the people (starring ti as the people)",
     "paul mccartney - fuh you",
-    "eminem - venom",
-    "r. kelly - i admit",
+    "eminem - venom - from the motion picture",
+    "r. kelly - i admit",  # not on spotify
     "tom macdonald - whiteboy",
 }
 
 worst_tracks_2019 = {
     "jeremy renner - heaven don't have a name",
-    "herve pagez & diplo - spicy ft. charli xcx",
-    "ed sheeran - i don't care ft. justin bieber",
+    "herve pagez & diplo & charlie xcx - spicy (with diplo & charli xcx)",
+    "ed sheeran & justin bieber - i don't care (with justin bieber)",
     "sleater-kinney - the future is here",
     "lil ak & 6ix9ine - lanes",
     "gunna - speed it up",
-    "miley cyrus - cattitude ft. rupaul",
-    "93punx - 3 years sober",
+    "miley cyrus & rupaul - cattitude (feat. rupaul)",
+    "93punx & vic mensa & travis barker - 3 years sober",
     "vin jay - mumble rapper vs. lyricist",
     "lil dicky - earth",
 }
@@ -59,45 +60,45 @@ worst_tracks_2019 = {
 worst_tracks_2020 = {
     "wolfmother - high on my own supply",
     "j. cole - snow on tha bluff",
-    "corey taylor - cmft must be stopped ft. tech n9ne & kid bookie",
-    "gawne x mac lethal x futuristic x crypt - death to mumble rap",
+    "corey taylor & tech n9ne & kid bookie - cmft must be stopped (feat. tech n9ne and kid bookie)",
+    "gawne & mac lethal & futuristic & crypt - death to mumble rap",
     "tones and i - ur so f**king cool",
     "psychosexual - let the sin begin",
-    "lil pump - lil pimp big maga steppin",
-    "farrah abraham - jingle bell rock",
+    "lil pump - lil pimp big maga steppin",  # not on spotify
+    "farrah abraham - jingle bell rock",  # not on spotify
     "van morrison - no more lockdown",
-    "justin bieber - yummy (country remix) ft. florida georgia line",
+    "justin bieber & florida georgia line - yummy - country remix",
 }
 
 worst_tracks_2021 = {
     "grimes - player of games",
-    "jimmy fallon - it was a… (masked christmas) ft. ariana grande & megan thee stallion",
-    "drake - way 2 sexy ft. future & young thug",
+    "jimmy fallon & ariana grande & megan thee stallion - it was a… (masked christmas) (feat. ariana grande & megan thee stallion)",
+    "drake & future & young thug - way 2 sexy (with future & young thug)",
     "machine gun kelly - papercuts",
     "walker hayes - fancy like",
     "tones and i - won't sleep",
     "ame bibabi - chin up high",
     "sarah brand - red dress",
-    "kid rock - don't tell me how to live ft. monster truck",
+    "kid rock & monster truck - don't tell me how to live",
     "eric clapton - this has gotta stop",
     "hobo johnson - i want to see the world",
-    "flyysoulja - i'm an island boy",
+    "flyysoulja & kodiyakredd - i'm an island boy",
     "guns n' roses - absuяd",
     "tramp stamps - i'd rather die",
     "corey feldman - rockin' revolution",
 }
 
 worst_tracks_2022 = {
-    "the notorious b.i.g.'s estate - the g.o.a.t. ft. ty dolla $ign & bella alubo",
+    "the notorious b.i.g. & ty dolla $ign & bella alubo - g.o.a.t. (feat. ty dolla $ign & bella alubo)",
     "lizzo - grrrls",
-    "machine gun kelly - ay! ft. lil wayne",
+    "machine gun kelly & lil wayne - ay! (feat. lil wayne)",
     "david guetta & bebe rexha - i'm good (blue)",
     "central cee - doja",
     "ghost - twenties",
     "muse - will of the people",
     "kid rock - we the people",
     "tom macdonald & adam calhoun - new world order",
-    "leah kate - twinkle twinkle little bitch",
+    "leah kate - twinkle twinkle",
 }
 
 worst_tracks_2023 = {
@@ -106,7 +107,7 @@ worst_tracks_2023 = {
     "kodak black - no love for a thug",
     "grimes & illangelo - i wanna be software",
     "meghan trainor - mother",
-    "forgiato blow - boycott target",
+    "forgiato blow & jimmy levy & nick nittoli & stoney dudebro - boycott target",
     "oliver anthony - rich men north of richmond",
     "falling in reverse - watch the world burn",
     "tx2 - i would hate me too",
@@ -115,23 +116,25 @@ worst_tracks_2023 = {
 
 worst_tracks_2010s = {
     "alison gold - chinese food",
+    "panda bear - i love chinese food",  # duplicate?
     "ajr - i'm ready",
     "the chainsmokers - #selfie",
-    "the black eyed peas - #wheresthelove",
+    "the black eyed peas & the world - #wheresthelove",
     "3oh!3 - my dick",
-    "jake paul - it's everyday bro",
-    "93punx - 3 years sober",
+    "jake paul & team 10 - it's everyday bro",
+    "93punx & vic mensa & travis barker - 3 years sober",
     "kim kardashian - jam (turn it up)",
-    "eminem - venom",
+    "eminem - venom - music from the motion picture",
     "raelynn - god made girls",
-    "corey feldman - go 4 it",
-    "neal mccoy - take a knee… my ass!!",
+    "corey feldman & snoop dogg - go 4 it ! (feat. snoop dogg)",
+    "neal mccoy - take a knee, my ass",  # not on spotify
     "vin jay - mumble rapper vs. lyricist",
-    "le tigre - i'm with her",
+    "le tigre - i'm with her",  # not on spotify
     "b.o.b - flatline",
+    "b.o.b. - flatline pt =",  # duplicate?
     "lil dicky - earth",
     "tom macdonald - whiteboy",
-    "r. kelly - i admit",
-    "your favorite martian - transphobic techno (bitch got a penis)",
+    "r. kelly - i admit",  # not on spotify
+    "your favorite martian - transphobic techno",
     "hopsin - happy ending",
 }
